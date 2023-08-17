db3d9f8698e7dea8eb97ddcc24ac275c9c3b87fb
Antoine Voituron <avoitu@orange.fr>
2023-08-18 00:44:23 +0200

    changement des variables en int pour permettre un calcul après input, fin de l'exercie avec la division par zéro et l'ajout du résultat final

--- exomatch.py
+++ exomatch.py
@@ -1,16 +1,21 @@
 print("Choissez un nombre entier")
 nombre_a_gauche=input()
 
+
 if nombre_a_gauche.isnumeric()==False :
   print("Erreur: les deux nombres doivent être des nombres entiers")
 
 
-print("Choissisez un nombre entier")
+nombre_a_gauche=int(nombre_a_gauche)
+
+print("Choissisez un autre nombre entier")
 nombre_a_droite=input()
 
 if nombre_a_droite.isnumeric()==False :
   print("Erreur: les deux nombres doivent être des nombres entiers")
   
+
+nombre_a_droite=int(nombre_a_droite)  
 
 print("Choissisez entre +, -, * ou /")
 operation=input()
@@ -18,9 +23,20 @@ operation=input()
 match operation:
   case "+" :
     résultat=(nombre_a_gauche+nombre_a_droite)
+    print(résultat)
   case "-" :
     résultat=(nombre_a_gauche-nombre_a_droite)
+    print(résultat)
   case "*" :
     résultat=(nombre_a_gauche*nombre_a_droite)
+    print(résultat)
   case "/" :
-    résultat=(nombre_a_gauche/nombre_a_droite)
+    if nombre_a_droite==0 :
+      print("Erreur: impossible de diviser par zéro.")
+      exit()
+    else :
+      résultat=(nombre_a_gauche/nombre_a_droite)
+      print(résultat)
+  case _ :
+    print("Erreur: le symbole d'opération doit être '+', '-', '*' ou '/'")
+    exit()
