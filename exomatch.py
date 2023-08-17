5bc5be05e618e8c914d668680989e6256d9fcd71
Antoine Voituron <avoitu@orange.fr>
2023-08-17 17:35:33 +0200

    initialisation de l'exo python sur les conditions et l'utilsation du match

---
+++ exomatch.py
@@ -0,0 +1,20 @@
+print("Choissez un nombre entier")
+nombre_a_gauche=input()
+
+if isnumeric(nombre_a_gauche)==False
+  print("Erreur: les deux nombres doivent être des nombres entiers")
+
+
+print("Choissisez un nombre entier")
+nombre_a_droite=input()
+
+if isnumeric(nombre_a_doite)==False
+  print("Erreur: les deux nombres doivent être des nombres entiers")
+  
+
+print("Choissisez entre +, -, * ou /")
+operation=input()
+
+match operation:
+  case "+"
+    print()
